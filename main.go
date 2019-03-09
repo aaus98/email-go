1638fd41cbc0ec29b1bf2626c783f0d114dcf477
adrian.ubillus <adrian.ubillus@2myr.com>
2019-03-09 18:28:02 -0500

    clean

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
-	"fmt"
 	"io"
-	"log"
-	"net/http"
 	"os"
 	"send_email_go/mailers"
 )
@@ -23,33 +19,13 @@ type Response struct {
 }
 
 func main() {
-	http.HandleFunc("/user", greet)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	name := query.Get("name")
-	email := query.Get("email")
-	fmt.Println(name)
-	fmt.Println(email)
-	// users, _ := parseUser("./user.csv")
-	// for _, user := range users {
-	// 	subject := "Prueba Sunapp es gratis"
-	// 	receiver := []string{user.Email}
-	// 	r := mailers.NewRequest(receiver, subject)
-	// 	r.Send("templates/template.html", map[string]string{"username": user.Name})
-	// }
-	subject := "Prueba Sunapp es gratis"
-	receiver := []string{email}
-	ra := mailers.NewRequest(receiver, subject)
-	ra.Send("templates/template.html", map[string]string{"username": name})
-	response := Response{}
-	response.Status = true
-	responseConvert, _ := json.Marshal(response)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(responseConvert)
-	// fmt.Fprintf(w, "Se envió correo a "+email+"")
+	users, _ := parseUser("./user.csv")
+	for _, user := range users {
+		subject := "Prueba Sunapp es gratis"
+		receiver := []string{user.Email}
+		r := mailers.NewRequest(receiver, subject)
+		r.Send("templates/template.html", map[string]string{"username": user.Name})
+	}
 }
 
 func parseUser(file string) (map[string]*User, error) {
